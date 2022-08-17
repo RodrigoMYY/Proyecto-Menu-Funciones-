05bbaa04125b9719aed54ec52baf608a506a6001
RodrigoMYY <106542028+RodrigoMYY@users.noreply.github.com>
2022-08-17 09:22:30 -0400

    Add files via upload

---
+++ Tareamenu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tareamenu;
+
+/**
+ *
+ * @author TP
+ */
+public class Tareamenu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
